b0e4c54af9a52cbe3e8b2ec587eb6d136d60353f
Uyen <ulam10@gmail.com>
2020-05-04 16:29:34 -0700

    updated readme,requirements, and settings.py;  added procfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn <virtualpets>.wsgi
--- virtualpets/virtualpets/settings.py
+++ virtualpets/virtualpets/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@p6r#%ada-a&#@gfcr^l5l7-x^7vyi=&p=(t8t5%r)n(*5rd-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
